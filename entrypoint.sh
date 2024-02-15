a0e2514af0369b876f22ce10a53e6ee00b5a7194
remiDebuisson <159796278+remiDebuisson@users.noreply.github.com>
2024-02-15 11:29:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash 
 # Démarrer Nginx en arrière-plan 
-nginx -g 'daemon off;' &  
-# Démarrer le serveur Symfony 
-symfony server:start --port=9000 --no-tls
+nginx -g 'daemon off;' &  symfony server:start --port=9000 --no-tls
 # Garder le conteneur en marche 
 tail -f /dev/null
